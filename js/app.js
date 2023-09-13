015299119a3defe90f54472aeb45e86f3615c854
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2023-09-13 15:46:19 +0200

    update app.js

--- js/app.js
+++ js/app.js
@@ -89,7 +89,8 @@ async function setup() {
       }
     };
     
-    const onGainChange = (e) => device.parametersById.get(e.currentTarget.name).normalizedValue = parseFloat(e.currentTarget.value);
+    // const onGainChange = (e) => device.parametersById.get(e.currentTarget.name).normalizedValue = parseFloat(e.currentTarget.value);
+    const onGainChange = (e) => device.parametersById.get(e.currentTarget.name) = parseFloat(e.currentTarget.value);
     
     document.querySelector("#start").onclick = run;
     document.querySelector("#gain").onchange = onGainChange;
